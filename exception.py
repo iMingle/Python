6e951565eb77aa8dcb2349f79c575d31f086806f
jinminglei <jinminglei@yeah.net>
2015-12-31 18:21:02 +0800

    exception

---
+++ exception.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2016, Mingle. All rights reserved.
+# Author: Mingle
+# Contact: jinminglei@yeah.net
+
+import time
+
+try:
+	text = input("Enter something --> ")
+except EOFError:
+	print('Why did you do an EOF on me?')
+except KeyboardInterrupt:
+	print('You cancelled the operation.')
+else:
+	print('You entered {0}'.format(text))
+
+class ShortInputException(Exception):
+	"""A user-defined exception class"""
+	def __init__(self, length, atleast):
+		super(ShortInputException, self).__init__()
+		self.length = length
+		self.atleast = atleast
+
+try:
+	text = input("Enter something --> ")
+	if len(text) < 3:
+		raise ShortInputException(len(text), 3)
+except EOFError:
+	print('Why did you do an EOF on me?')
+except ShortInputException as ex:
+	print('ShortInputException The input was {0} long, excepted atleast {1}'.format(ex.length, ex.atleast))
+else:
+	print('No exception was raised.')
+
+try:
+	f = open("poem.txt")
+	while True:
+		line = f.readline()
+		if len(line) == 0:
+			break
+		print(line, end = '')
+		time.sleep(2)
+except KeyboardInterrupt:
+	print('!! You cancelled the reading from the file.')
+finally:
+	f.close()
+	print('(Cleanig up: closed the file)')
