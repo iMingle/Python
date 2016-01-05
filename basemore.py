d1b8d961ab4d7e6232426642af894bade9cbb8d6
jinminglei <jinminglei@yeah.net>
2016-01-05 18:41:09 +0800

    list oper, exec, eval, repr, assert

--- basemore.py
+++ basemore.py
@@ -28,6 +28,40 @@ twice = make_repeater(2)
 print(twice("work"))
 print(twice(5))
 
-points = [{"x": 2, "y": 3}, {"x": 4, "y": 1}]
-points.sort(lambda x, y : cmp(x["x"], y["x"]), key = x)
+points = [{"x": 2, "y": 3}, {"x": 4, "y": 1}, {"x": 0, "y": 1}, {"x": 1, "y": 1}]
+points.sort(key = lambda point : point["x"]);
 print(points)
+
+# 列表综合
+listone = [2, 3, 4]
+listtwo = [2*i for i in listone if i > 2]
+print(listtwo)
+
+def powersum(power, *args):
+	'''Return the sum of each argument raised to specified power.'''
+	total = 0
+	for i in args:
+		total += pow(i, power)
+	return total
+
+print(powersum(2, 3, 4))
+print(powersum(2, 10))
+
+# exec语句用来执行储存在字符串或文件中的Python语句
+exec("print('hello')")
+# eval函数用来执行存储在字符串中的Python表达式
+print(eval('2*3'))
+
+# assert
+list = ["item"]
+assert len(list) >= 1
+list.pop()
+# assert len(list) >= 1
+
+# repr函数用来取得对象的规范字符串表示,注意,在大多数时候有eval(repr(object)) == object.
+i = []
+i.append("item")
+print(i)
+print(repr(i))
+print(eval(repr(i)))
+print(eval(repr(i)) == i)
